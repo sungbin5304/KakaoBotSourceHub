6444f29c2694ec29c034c9add925964d4fd0d7fa
sungbin5304 <sungbin.dev@gmail.com>
2020-08-21 21:25:08 +0900

    refactoring - apply kotlin-dsl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,2 @@
-import de.fayard.dependencies.bootstrapRefreshVersionsAndDependencies
-
 include(":app")
 rootProject.name="KakaoBotSourceHub"
-
-buildscript {
-    repositories { gradlePluginPortal() }
-    dependencies.classpath("de.fayard:dependencies:0.5.7")
-}
-
-bootstrapRefreshVersionsAndDependencies()
